80a29c153f2bff0907711be902c1e25b1a606457
Shawon-Lodh <50014582+Shawon-Lodh@users.noreply.github.com>
2021-12-19 18:03:52 +0600

    initial update

--- lib/model/transaction.dart
+++ lib/model/transaction.dart
@@ -7,12 +7,23 @@ class Transaction extends HiveObject {
   @HiveField(0)
   late String name;
 
+  get nameValue{
+    return name;
+  }
+
+  set waterParametersDataLoadValue(String valueName) {
+    name = valueName;
+  }
+
   @HiveField(1)
   late DateTime createdDate;
+
 
   @HiveField(2)
   late bool isExpense = true;
 
   @HiveField(3)
   late double amount;
+
+
 }
